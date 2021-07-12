6bf2e8a3d9fcd76a8bf00dd9be74eec41b86e396
Hien Do <ddhien@cmc.com.vn>
2021-07-12 15:42:50 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container">
       <PolicyHolderDetail v-if="$formData.currentStep === 1"/>
-      <EvolutionHealthPackage v-if="$formData.currentStep === 2" />
+      <EvolutionHealth v-if="$formData.currentStep === 2" />
       <AdditionalDetails v-if="$formData.currentStep === 3" />
     </div>
   </div>
@@ -10,14 +10,14 @@
 
 <script>
 import PolicyHolderDetail from "./components/PolicyHolderDetails/PolicyHolderDetail";
-import EvolutionHealthPackage from "./components/SelectQuote/EvolutionHealthPackage";
+import EvolutionHealth from "./components/SelectQuote/EvolutionHealth";
 import AdditionalDetails from "./components/AdditionalDetails/AdditionalDetails";
 
 export default {
   name: "App",
   components: {
     PolicyHolderDetail,
-    EvolutionHealthPackage,
+    EvolutionHealth,
     AdditionalDetails,
   },
   data(){
