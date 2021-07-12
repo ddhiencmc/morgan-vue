28711553a28ad92e787d6fce10c79bfdcc6a63a6
Hien Do <ddhien@cmc.com.vn>
2021-07-12 14:22:53 +0700

    update vue.config

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    css: {
+        extract: true,
+        loaderOptions: {
+            css: {
+
+            }
+        }
+    },
+    // options...
+    configureWebpack: {
+        output: {
+            libraryExport: 'default',
+            filename: '[name].bundle.js',
+            chunkFilename: 'app.js',
+        }
+    },
+}
