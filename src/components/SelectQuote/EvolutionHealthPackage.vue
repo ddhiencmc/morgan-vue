f3e608e692a774946b039e0a2fdc7d731d5611bb
Hien Do <ddhien@cmc.com.vn>
2021-07-12 15:37:25 +0700

    Update style

--- src/components/SelectQuote/EvolutionHealthPackage.vue
+++ src/components/SelectQuote/EvolutionHealthPackage.vue
@@ -1,146 +1,148 @@
 <template>
   <div class="">
-    <!-- info -->
-    <div class="">
-      <h2 class="text-center text-primary">Evolution Health ROW</h2>
+    <h2 class="text-center text-primary">Evolution Health ROW</h2>
 
-      <div class="Form border-top border-bottom mx-auto">
-        <div class="m-2">
-          <button class="btn btn-light border-primary ml-1 m-1 fs-15px">
-            Europe
-          </button>
+    <div class="border-top border-bottom">
+      <div class="p-2 text-center">
+        <button class="btn btn-light border-primary ml-1 m-1 fs-15px">
+          Europe
+        </button>
 
-          <button class="btn btn-primary border-primary m-1 fs-15px">
-            Worldwide (excluding USA, China, Singapore and Hong Kong)
-          </button>
+        <button class="btn btn-primary border-primary m-1 fs-15px">
+          Worldwide (excluding USA, China, Singapore and Hong Kong)
+        </button>
 
-          <button class="btn btn-light border-primary m-1 fs-15px">
-            Worldwide (excluidng USA)
-          </button>
+        <button class="btn btn-light border-primary m-1 fs-15px">
+          Worldwide (excluidng USA)
+        </button>
 
-          <button class="btn btn-light border-primary mr-1 m-1 fs-15px">
-            Worldwide
-          </button>
-        </div>
+        <button class="btn btn-light border-primary mr-1 m-1 fs-15px">
+          Worldwide
+        </button>
       </div>
+    </div>
 
-      <div class="d-flex justify-content-between fs-15px">
-        <!-- package -->
+    <div class="d-flex justify-content-between fs-15px">
+      <div
+        class="mx-1 w-20"
+        v-for="(service, index) in services"
+        :key="index"
+      >
+        <div class="container bg-light py-3 mt-3 mb-2 text-center fs-20px">
+          <strong>{{ service.name }}</strong>
+        </div>
+
         <div
-          class="mx-1 w-20"
-          v-for="(service, index) in services"
-          :key="index"
+          class="
+            container
+            bg-light
+            m-1
+            px-2
+            h-100
+            d-flex
+            flex-column
+            justify-content-between
+            pb-3
+            px-4
+          "
         >
-          <!-- title -->
-          <div class="container bg-light py-3 mt-3 mb-2 text-center fs-20px ">
-            <strong>{{ service.name }}</strong>
-          </div>
+          <div>
+            <div class="my-3 text-center">
+              Choose an Excess
+              <i
+                class="fa fa-question-circle"
+                aria-hidden="true"
+                data-bs-toggle="tooltip"
+                title="Description"
+              ></i>
+            </div>
 
-          <!-- body -->
-          <div
-            class="container bg-light m-1 px-2 h-100 d-flex flex-column justify-content-between pb-3 px-4"
-          >
-            <div>
-              <div class="my-3">
-                Choose an Excess
-                <!-- bi bi-question-circle -->
-                <i
-                  class="fa fa-question-circle"
-                  aria-hidden="true"
-                  data-bs-toggle="tooltip"
-                  title="Question"
-                ></i>
-              </div>
+            <select
+              class="form-select mb-3 fs-15px"
+              aria-label="Default select example"
+            >
+              <option selected>0 per person per</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
 
-              <select
-                class="form-select mb-3 fs-15px"
-                aria-label="Default select example"
-              >
-                <option selected>0 per person per</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-
-              <!-- CoInsurance radio -->
-              <div v-if="!service.noCoInsurance">
-                <div class="mb-2 text-center">
-                  Co-insurance
-                  <i
-                    class="fa fa-question-circle border-0"
-                    aria-hidden="true"
-                    data-bs-toggle="tooltip"
-                    title="Question"
-                  ></i>
-                </div>
-
-                <!-- radio -->
-                <div
-                  class="form-check"
-                  v-for="(insuranceOption, insuranceIndex) in insuarancesOption"
-                  :key="insuranceIndex"
-                >
-                  <input
-                    class="form-check-input"
-                    type="radio"
-                    :name="'insuranceRadio_' + index"
-                    :id="'insurance_' + index + insuranceIndex"
-                  />
-                  <label
-                    class="form-check-label"
-                    :for="'insurance_' + index + insuranceIndex"
-                  >
-                    {{ insuranceOption }}
-                  </label>
-                </div>
-              </div>
-
-              <div class="text-center my-3">
-                Additional Benefits
+            <div v-if="!service.noCoInsurance">
+              <div class="mb-2 text-center">
+                Co-insurance
                 <i
                   class="fa fa-question-circle border-0"
                   aria-hidden="true"
                   data-bs-toggle="tooltip"
-                  title="Question"
+                  title="Description"
                 ></i>
               </div>
 
-              <div class="form-check">
+              <div
+                class="form-check mt-2"
+                v-for="(insuranceOption, insuranceIndex) in insuarancesOption"
+                :key="insuranceIndex"
+              >
                 <input
                   class="form-check-input"
-                  type="checkbox"
-                  value=""
-                  :id="'homeCountryEvacuation_' + index"
+                  type="radio"
+                  :name="'insuranceRadio_' + index"
+                  :id="'insurance_' + index + insuranceIndex"
                 />
                 <label
                   class="form-check-label"
-                  :for="'homeCountryEvacuation_' + index"
+                  :for="'insurance_' + index + insuranceIndex"
                 >
-                  Home country Evacuation
+                  {{ insuranceOption }}
                 </label>
               </div>
             </div>
 
-            <!-- frequencyOptions -->
-            <select
-              class="form-select"
-              aria-label="Default select example"
-              v-model="service.frequency"
-            >
-              <option selected>Select package</option>
+            <div class="text-center mt-4">
+              Additional Benefits
+              <i
+                class="fa fa-question-circle border-0"
+                aria-hidden="true"
+                data-bs-toggle="tooltip"
+                title="Description"
+              ></i>
+            </div>
 
-              <option
-                v-for="option in service.frequencyOptions"
-                :key="option.type"
-                value="option.type"
-                >{{ `${option.type}: ${option.price}` }}</option
+            <div class="form-check mt-2">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                :id="'homeCountryEvacuation_' + index"
+              />
+              <label
+                class="form-check-label"
+                :for="'homeCountryEvacuation_' + index"
               >
-            </select>
+                Home country Evacuation
+              </label>
+            </div>
           </div>
+
+          <select
+            class="form-select"
+            aria-label="Default select example"
+            v-model="service.frequency"
+          >
+            <option selected>Select package</option>
+
+            <option
+              v-for="option in service.frequencyOptions"
+              :key="option.type"
+              value="option.type"
+            >
+              {{ `${option.type}: ${option.price}` }}
+            </option>
+          </select>
         </div>
       </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -157,10 +159,12 @@ export default {
     };
   },
   mounted() {
-    let tooltipTriggerList = [].slice.call(this.$el.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    let tooltipTriggerList = [].slice.call(
+      this.$el.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
     tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new window.bootstrap.Tooltip(tooltipTriggerEl)
-    })
+      return new window.bootstrap.Tooltip(tooltipTriggerEl);
+    });
   },
 };
 </script>
