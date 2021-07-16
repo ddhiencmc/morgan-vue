309da54a29b41ae0f7350696a610708d8df139fa
Hien Do <ddhien@cmc.com.vn>
2021-07-16 17:52:14 +0700

    Remove state

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import formData from './data/formData'
-import state from './data/state'
 import '@/utils/validation.js'
 import 'vue-multiselect/dist/vue-multiselect.min.css'
 import '@/styles/index.scss'
 
 Vue.config.productionTip = false
-
 Vue.prototype.$formData = formData
-Vue.prototype.$state = state
 
 new Vue({
   render: h => h(App),
